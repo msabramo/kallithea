425183acf1f3441e2319928b9a399655c0eb9442
Marcin Kuźmiński <marcinkuzminski@users.noreply.github.com>
2011-03-11 15:56:52 +0100

    sorted tags by date in tag view

---
+++ docs/installation.rst
@@ -0,0 +1,106 @@
+.. _installation:
+
+Installation
+============
+
+``RhodeCode`` is written entirely in Python. In order to gain maximum performance
+there are some third-party you must install. When RhodeCode is used 
+together with celery you have to install some kind of message broker,
+recommended one is rabbitmq_ to make the async tasks work.
+
+Of course RhodeCode works in sync mode also and then you do not have to install
+any third party applications. However, using Celery_ will give you a large 
+speed improvement when using many big repositories. If you plan to use 
+RhodeCode for say 7 to 10 small repositories, RhodeCode will perform perfectly 
+well without celery running.
+   
+If you make the decision to run RhodeCode with celery make sure you run 
+celeryd using paster and message broker together with the application.   
+
+Installing RhodeCode from Cheese Shop
+-------------------------------------
+
+Rhodecode requires python version 2.5 or higher.
+
+The easiest way to install ``rhodecode`` is to run::
+
+    easy_install rhodecode
+
+Or::
+
+    pip install rhodecode
+
+If you prefer to install RhodeCode manually simply grab latest release from
+http://pypi.python.org/pypi/rhodecode, decompress the archive and run::
+
+    python setup.py install
+
+
+Step by step installation example
+---------------------------------
+
+
+- Assuming you have installed virtualenv_ create a new virtual environment using virtualenv:: 
+
+    virtualenv --no-site-packages /var/www/rhodecode-venv
+
+
+.. note:: Using ``--no-site-packages`` when generating your
+   virtualenv is **very important**. This flag provides the necessary
+   isolation for running the set of packages required by
+   RhodeCode.  If you do not specify ``--no-site-packages``,
+   it's possible that RhodeCode will not install properly into
+   the virtualenv, or, even if it does, may not run properly,
+   depending on the packages you've already got installed into your
+   Python's "main" site-packages dir.
+
+
+- this will install new virtualenv_ into `/var/www/rhodecode-venv`. 
+- Activate the virtualenv_ by running::
+
+    source /var/www/rhodecode-venv/bin/activate
+
+.. note:: If you're using UNIX, *do not* use ``sudo`` to run the
+   ``virtualenv`` script.  It's perfectly acceptable (and desirable)
+   to create a virtualenv as a normal user.
+     
+- Make a folder for rhodecode somewhere on the filesystem for example::
+
+    mkdir /var/www/rhodecode
+  
+    
+- Run this command to install rhodecode::
+
+    easy_install rhodecode 
+
+- This will install rhodecode together with pylons and all other required python
+  libraries
+
+Requirements for Celery (optional)
+----------------------------------
+
+.. note::
+   Installing message broker and using celery is optional, RhodeCode will
+   work perfectly fine without them.
+
+
+**Message Broker** 
+
+- preferred is `RabbitMq <http://www.rabbitmq.com/>`_
+- A possible alternative is `Redis <http://code.google.com/p/redis/>`_
+
+For installation instructions you can visit: 
+http://ask.github.com/celery/getting-started/index.html.
+This is a very nice tutorial on how to start using celery_ with rabbitmq_
+
+
+You can now proceed to :ref:`setup`
+-----------------------------------
+
+
+
+.. _virtualenv: http://pypi.python.org/pypi/virtualenv  
+.. _python: http://www.python.org/
+.. _mercurial: http://mercurial.selenic.com/
+.. _celery: http://celeryproject.org/
+.. _rabbitmq: http://www.rabbitmq.com/
---
+++ rhodecode/controllers/login.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+    rhodecode.controllers.login
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Login controller for rhodeocode
+    
+    :created_on: Apr 22, 2010
+    :author: marcink
+    :copyright: (C) 2009-2011 Marcin Kuzminski <marcin@python-works.com>    
+    :license: GPLv3, see COPYING for more details.
+"""
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2
+# of the License or (at your opinion) any later version of the license.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
+import logging
+import formencode
+
+from formencode import htmlfill
+
+from pylons.i18n.translation import _
+from pylons.controllers.util import abort, redirect
+from pylons import request, response, session, tmpl_context as c, url
+
+import rhodecode.lib.helpers as h
+from rhodecode.lib.auth import AuthUser, HasPermissionAnyDecorator
+from rhodecode.lib.base import BaseController, render
+from rhodecode.model.forms import LoginForm, RegisterForm, PasswordResetForm
+from rhodecode.model.user import UserModel
+
+
+log = logging.getLogger(__name__)
+
+class LoginController(BaseController):
+
+    def __before__(self):
+        super(LoginController, self).__before__()
+
+    def index(self):
+        #redirect if already logged in
+        c.came_from = request.GET.get('came_from', None)
+
+        if self.rhodecode_user.is_authenticated \
+                            and self.rhodecode_user.username != 'default':
+
+            return redirect(url('home'))
+
+        if request.POST:
+            #import Login Form validator class
+            login_form = LoginForm()
+            try:
+                c.form_result = login_form.to_python(dict(request.POST))
+                #form checks for username/password, now we're authenticated
+                username = c.form_result['username']
+                user = UserModel().get_by_username(username,
+                                                   case_insensitive=True)
+                auth_user = AuthUser(user.user_id)
+                auth_user.set_authenticated()
+                session['rhodecode_user'] = auth_user
+                session.save()
+
+                log.info('user %s is now authenticated and stored in session',
+                         username)
+                user.update_lastlogin()
+
+                if c.came_from:
+                    return redirect(c.came_from)
+                else:
+                    return redirect(url('home'))
+
+            except formencode.Invalid, errors:
+                return htmlfill.render(
+                    render('/login.html'),
+                    defaults=errors.value,
+                    errors=errors.error_dict or {},
+                    prefix_error=False,
+                    encoding="UTF-8")
+
+        return render('/login.html')
+
+    @HasPermissionAnyDecorator('hg.admin', 'hg.register.auto_activate',
+                               'hg.register.manual_activate')
+    def register(self):
+        user_model = UserModel()
+        c.auto_active = False
+        for perm in user_model.get_by_username('default', cache=False).user_perms:
+            if perm.permission.permission_name == 'hg.register.auto_activate':
+                c.auto_active = True
+                break
+
+        if request.POST:
+
+            register_form = RegisterForm()()
+            try:
+                form_result = register_form.to_python(dict(request.POST))
+                form_result['active'] = c.auto_active
+                user_model.create_registration(form_result)
+                h.flash(_('You have successfully registered into rhodecode'),
+                            category='success')
+                return redirect(url('login_home'))
+
+            except formencode.Invalid, errors:
+                return htmlfill.render(
+                    render('/register.html'),
+                    defaults=errors.value,
+                    errors=errors.error_dict or {},
+                    prefix_error=False,
+                    encoding="UTF-8")
+
+        return render('/register.html')
+
+    def password_reset(self):
+        user_model = UserModel()
+        if request.POST:
+
+            password_reset_form = PasswordResetForm()()
+            try:
+                form_result = password_reset_form.to_python(dict(request.POST))
+                user_model.reset_password(form_result)
+                h.flash(_('Your new password was sent'),
+                            category='success')
+                return redirect(url('login_home'))
+
+            except formencode.Invalid, errors:
+                return htmlfill.render(
+                    render('/password_reset.html'),
+                    defaults=errors.value,
+                    errors=errors.error_dict or {},
+                    prefix_error=False,
+                    encoding="UTF-8")
+
+        return render('/password_reset.html')
+
+    def logout(self):
+        del session['rhodecode_user']
+        session.save()
+        log.info('Logging out and setting user as Empty')
+        redirect(url('home'))
