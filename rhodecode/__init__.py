8e7aac0bdf31dd9be898a4f579f67b5933fe4c38
Marcin Kuźmiński <marcinkuzminski@users.noreply.github.com>
2011-12-28 23:57:57 +0200

    Added instruction on enabling the API access to web views

---
+++ rhodecode/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+    rhodecode.__init__
+    ~~~~~~~~~~~~~~~~~~
+
+    RhodeCode, a web based repository management based on pylons
+    versioning implementation: http://semver.org/
+
+    :created_on: Apr 9, 2010
+    :author: marcink
+    :copyright: (C) 2009-2011 Marcin Kuzminski <marcin@python-works.com>
+    :license: GPLv3, see COPYING for more details.
+"""
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import platform
+
+VERSION = (1, 3, 0, 'beta')
+__version__ = '.'.join((str(each) for each in VERSION[:4]))
+__dbversion__ = 4  # defines current db version for migrations
+__platform__ = platform.system()
+__license__ = 'GPLv3'
+
+PLATFORM_WIN = ('Windows')
+PLATFORM_OTHERS = ('Linux', 'Darwin', 'FreeBSD', 'OpenBSD', 'SunOS')
+
+try:
+    from rhodecode.lib import get_current_revision
+    _rev = get_current_revision()
+except ImportError:
+    # this is needed when doing some setup.py operations
+    _rev = False
+
+if len(VERSION) > 3 and _rev:
+    __version__ += ' [rev:%s]' % _rev[0]
+
+
+def get_version():
+    """Returns shorter version (digit parts only) as string."""
+
+    return '.'.join((str(each) for each in VERSION[:3]))
+
+BACKENDS = {
+    'hg': 'Mercurial repository',
+    'git': 'Git repository',
+}
+
+CELERY_ON = False
+
+# link to config for pylons
+CONFIG = None
---
+++ rhodecode/controllers/admin/admin.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+    rhodecode.controllers.admin.admin
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Controller for Admin panel of Rhodecode
+
+    :created_on: Apr 7, 2010
+    :author: marcink
+    :copyright: (C) 2009-2011 Marcin Kuzminski <marcin@python-works.com>
+    :license: GPLv3, see COPYING for more details.
+"""
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+
+from pylons import request, tmpl_context as c
+from sqlalchemy.orm import joinedload
+from webhelpers.paginate import Page
+
+from rhodecode.lib.auth import LoginRequired, HasPermissionAllDecorator
+from rhodecode.lib.base import BaseController, render
+from rhodecode.model.db import UserLog
+
+log = logging.getLogger(__name__)
+
+
+class AdminController(BaseController):
+
+    @LoginRequired()
+    def __before__(self):
+        super(AdminController, self).__before__()
+
+    @HasPermissionAllDecorator('hg.admin')
+    def index(self):
+
+        users_log = self.sa.query(UserLog)\
+                .options(joinedload(UserLog.user))\
+                .options(joinedload(UserLog.repository))\
+                .order_by(UserLog.action_date.desc())
+
+        p = int(request.params.get('page', 1))
+        c.users_log = Page(users_log, page=p, items_per_page=10)
+        c.log_data = render('admin/admin_log.html')
+
+        if request.environ.get('HTTP_X_PARTIAL_XHR'):
+            return c.log_data
+        return render('admin/admin.html')
