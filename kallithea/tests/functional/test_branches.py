70d5e511c08da268133a186f33003c6a19bf11cf
Mads Kiilerich <madski@unity3d.com>
2013-06-12 12:23:20 +0200

    allover: let tags and bookmarks link to the changeset - not to its files

--- kallithea/tests/functional/test_branches.py
+++ kallithea/tests/functional/test_branches.py
@@ -7,7 +7,7 @@ class TestBranchesController(TestController):
         self.log_user()
         response = self.app.get(url(controller='branches',
                                     action='index', repo_name=HG_REPO))
-        response.mustcontain("""<a href="/%s/files/96507bd11ecc815ebc6270fdf6db110928c09c1e/">default</a>""" % HG_REPO)
+        response.mustcontain("""<a href="/%s/changeset/96507bd11ecc815ebc6270fdf6db110928c09c1e">default</a>""" % HG_REPO)
 
         # closed branches
         response.mustcontain("""<a href="/%s/changeset/95ca6417ec0de6ac3bd19b336d7b608f27b88711">git [closed]</a><""" % HG_REPO)
@@ -17,4 +17,4 @@ class TestBranchesController(TestController):
         self.log_user()
         response = self.app.get(url(controller='branches',
                                     action='index', repo_name=GIT_REPO))
-        response.mustcontain("""<a href="/%s/files/5f2c6ee195929b0be80749243c18121c9864a3b3/">master</a>""" % GIT_REPO)
+        response.mustcontain("""<a href="/%s/changeset/5f2c6ee195929b0be80749243c18121c9864a3b3">master</a>""" % GIT_REPO)
