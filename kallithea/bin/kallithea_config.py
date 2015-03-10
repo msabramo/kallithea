4cd7abc650e42d4c80a92eca140c6595909bc828
Mads Kiilerich <madski@unity3d.com>
2015-03-11 00:24:25 +0100

    bin: give kallithea_config.py #!/usr/bin/env python so it can be executed directly

--- kallithea/bin/kallithea_config.py
+++ kallithea/bin/kallithea_config.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
